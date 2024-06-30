a663a2e58ed875775fbfed527adb514d39876634
Rikako921 <rico.kyg@icloud.com>
2024-06-30 14:19:42 +0900

    ログイン・新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         validates :name, presence: true
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :kanafirst_name, presence: true
+         validates :kanalast_name, presence: true
+         validates :birthday, presence: true
+
+         
+        end
