68f2368b45ac9fb2d0d1a1f15410f9ff28a4c23b
Rikako921 <rico.kyg@icloud.com>
2024-07-02 19:08:05 +0900

    選択機能 viewに追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,11 +8,12 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true
+  validates :contents, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :shippingn_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :schedule_id, numericality: { other_than: 1 , message: "can't be blank"} 
-
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
