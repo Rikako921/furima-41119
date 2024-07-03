df2f9967707a6db03ceed771d78c7aa467f985fd
Rikako921 <rico.kyg@icloud.com>
2024-07-03 18:24:05 +0900

    javascript 設定

---
+++ app/javascript/post_price.js
@@ -0,0 +1,12 @@
+function pullDown() {
+  
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const minusTaxDom = document.getElementById("profit");
+    addTaxDom.innerHTML = Math.floor(inputValue / 10);
+    minusTaxDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+  });
+}
+window.addEventListener('load', pullDown)
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :contents, presence: true
-
+  validates :image, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_id, numericality: { other_than: 1 , message: "can't be blank"} 
