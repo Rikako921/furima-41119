0a1a5c1c19534acca94519e33915f2bfeb68956c
Rikako921 <rico.kyg@icloud.com>
2024-07-09 14:46:08 +0900

    コントローラー修正後

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user,only: [:edit, :update]
+  before_action :correct_user,only: [:edit, :update, :destroy]
   def index
     @posts = Post.all.order("created_at DESC")
   end
