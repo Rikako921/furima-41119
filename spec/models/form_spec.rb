f369a69591866e5a03357deba1dd8c3c4f207889
Rikako921 <rico.kyg@icloud.com>
2024-07-12 19:56:49 +0900

    テストコード編集

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Form, type: :model do
       it '郵便番号が「3桁-4桁」の半角文字列でなければ保存できないこと' do
         @form.postal_code = '1234567'
         expect(@form).to_not be_valid
-        expect(@form.errors.full_messages).to include("Postal code は「3桁-4桁」の半角文字列で入力してください")
+        expect(@form.errors.full_messages).to include("Postal code is invalid")
       end
       it '都道府県が1では保存できないこと' do
         @form.prefecture_id = 1
@@ -50,17 +50,17 @@ RSpec.describe Form, type: :model do
       it '電話番号が10桁未満では保存できないこと' do
         @form.tell_number = '090123456'
         expect(@form).to_not be_valid
-        expect(@form.errors.full_messages).to include("Tell number は10桁以上11桁以内の半角数値で入力してください")
+        expect(@form.errors.full_messages).to include("Tell number is invalid")
       end
       it '電話番号が12桁以上では保存できないこと' do
         @form.tell_number = '090123456789'
         expect(@form).to_not be_valid
-        expect(@form.errors.full_messages).to include("Tell number は10桁以上11桁以内の半角数値で入力してください")
+        expect(@form.errors.full_messages).to include("Tell number is invalid")
       end
       it '電話番号にハイフンが含まれていると保存できないこと' do
         @form.tell_number = '090-1234-5678'
         expect(@form).to_not be_valid
-        expect(@form.errors.full_messages).to include("Tell number は10桁以上11桁以内の半角数値で入力してください")
+        expect(@form.errors.full_messages).to include("Tell number is invalid")
       end
       it 'user_idが紐づいていなければ購入できないこと' do
         @form.user_id = ''
@@ -73,9 +73,9 @@ RSpec.describe Form, type: :model do
         expect(@form.errors.full_messages).to include("Post can't be blank")
       end
       it "tokenが空では登録できないこと" do
-        @order.token = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Token can't be blank")
+        @form.token = nil
+        @form.valid?
+        expect(@form.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
