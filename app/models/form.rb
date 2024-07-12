9f72bf0ed1449bcc9ea2c6fa1b6297b1dc1600ad
Rikako921 <rico.kyg@icloud.com>
2024-07-12 21:26:02 +0900

    with_optionsでまとめる

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,16 +2,18 @@ class Form
   include ActiveModel::Model
   attr_accessor  :token, :user_id, :post_id,:postal_code, :prefecture_id, :city, :street, :building, :tell_number
 
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :city
+    validates :street, presence: true
+    validates :tell_number, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id 
+    validates :post_id
+    validates :token
+  end
+
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :city, presence: true
-  validates :street, presence: true
-  validates :tell_number, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :user_id, presence: true
-  validates :post_id, presence: true
-  validates :token, presence: true
-
-
 
   def save
     order = Order.create(user_id: user_id, post_id: post_id)
